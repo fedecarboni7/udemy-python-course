b6de9df3e5abf82c91d8481605f9ef725c10acf8
fedecarboni7 <fedecarboni7@gmail.com>
2020-12-23 17:54:12 -0300

    data types

---
+++ Section 2/data_types.py
@@ -0,0 +1,15 @@
+#type conversions
+
+#float to int
+print(int(3.5))
+
+#int to float
+print(float(10))
+
+#boolean to int
+print(f"""{int(True)}
+{int(False)}""")
+
+#boolean to str
+print(f"""{str(True)}
+{str(False)}""")
