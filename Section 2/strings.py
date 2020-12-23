fb1facd58854280e82b10cfd03c952b15580f4bd
fedecarboni7 <fedecarboni7@gmail.com>
2020-12-22 23:15:44 -0300

    strings

---
+++ Section 2/strings.py
@@ -0,0 +1,15 @@
+#print function
+
+print("una cadena de texto \t separada por un tab \ny una segunda linea")
+
+#print raw (r)
+
+print("hola con \newline")
+print(r"hola sin \newline")
+
+#print """
+
+print(r"""forma copada de hacer
+varias lineas
+sin necesidad de
+poner \n""")
