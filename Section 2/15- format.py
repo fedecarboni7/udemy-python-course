56259941a8a80b6243ace40e66ea11de83cdfc2d
fedecarboni7 <fedecarboni7@gmail.com>
2021-01-04 17:46:25 -0300

    2.9 formateo

---
+++ Section 2/15- format.py
@@ -0,0 +1,14 @@
+## .format
+print("{} es el primer valor y {} es el segundo".format(1,2))
+
+print(("{1} es mas chico que {0} y mas grande que {2}").format(2, 1, 0))
+
+print("la edad de {} es {}".format("Fede", 21))
+
+## print(f)
+nombre = "fede"
+apellido = "carboni"
+edad = 21
+print(f"la edad de {nombre} es {edad}")
+
+print(f"el apellido de {nombre.title()} tiene {len(apellido)} letras")
