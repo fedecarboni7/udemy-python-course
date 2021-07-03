d3264a28500bd11ebd92cdbd94ad243bb32bea74
fedecarboni7 <fedecarboni7@gmail.com>
2021-07-03 16:16:39 -0300

    agregamos una funcion para calcular la edad

--- poo_perro.py
+++ poo_perro.py
@@ -1,5 +1,8 @@
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from datetime import date
+
+def calculate_age(anio, mes, dia):
+    today = date.today()
+    return today.year - anio - ((today.month, today.day) < (mes, dia))
 
 class Perro:
     hambre = True
@@ -8,6 +11,7 @@ class Perro:
         self.nombre = nombre
         self.raza = raza
         self.fecha_de_nacimiento = fecha_de_nacimiento
+        self.edad = None
         print("Se encontró un perro.\n")
 
     def nombrar(self):
@@ -18,43 +22,47 @@ class Perro:
 
     def calcular_edad(self):
         self.fecha_de_nacimiento = input("Ingresa la fecha de nacimiento (dd/mm/aaaa)\n")
-        dia, mes, año = self.fecha_de_nacimiento.split("/")
-        self.edad = relativedelta(datetime.now(), datetime(int(año), int(mes), int(dia))).years
+        dia, mes, anio = self.fecha_de_nacimiento.split("/")
+        self.edad = calculate_age(int(anio), int(mes), int(dia))
 
     def alimentar(self):
         self.hambre = False
+        print("Guau guau, gracias")
 
 def main():
     print("\nPrograma para tener un perro:")
-    perro = Perro("Odi", "Chihuahua")
+    perro = Perro()
     print("Para conocer el estado de tu perro, ingresa alguna de las siguientes opciones:")
-    opcion = int(input("1. Raza\n2. Edad\n3. Nombre\n4. Hambre\n5. Salir\nIngresar opción: "))
+    opcion = int(input("1. Nombre\n2. Edad\n3. Raza\n4. Hambre\n5. Salir\nIngresar opción: "))
     while opcion != 5:
         if opcion == 1:
-            print(perro.raza)
-            if perro.raza == None:
-                perro.definir_raza()
-        elif opcion == 2:
-            print(perro.edad)
-            if perro.edad == None:
-                perro.calcular_edad()
-        elif opcion == 3:
-            print(perro.nombre)
             if perro.nombre == None:
                 perro.nombrar()
+            else:
+                print(perro.nombre)
+        elif opcion == 2:
+            if perro.edad == None:
+                perro.calcular_edad()
+            else:
+                print(perro.edad, "años")
+        elif opcion == 3:
+            if perro.raza == None:
+                perro.definir_raza()
+            else:
+                print(perro.raza)
         elif opcion == 4:
             if perro.hambre:
                 print("\nTu perro tiene hambre.")
-                dar_comida = int(input("Presioná 1 para alimentarlo, o 0 para no\n"))
-                if dar_comida == 1:
+                opcion = int(input("Presioná 1 para alimentarlo, o 0 para no\n"))
+                if opcion == 1:
                     perro.alimentar()
-                elif dar_comida == 0:
+                elif opcion == 0:
                     print("Acordate de darle comida después")
                 else:
                     print("Tenías que tocar 0 o 1")
             else:
                 print("Sin hambre por ahora.")
-        opcion = int(input("\n1. Raza\n2. Edad\n3. Nombre\n4. Hambre\n5. Salir\nIngresar opción: "))
+        opcion = int(input("\n1. Nombre\n2. Edad\n3. Raza\n4. Hambre\n5. Salir\nIngresar opción: "))
     print("\nNos vemos che")
     
 
